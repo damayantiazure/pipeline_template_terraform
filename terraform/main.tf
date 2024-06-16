417aded583d53e606ad28083e9262801ab94ebcc
damayantiazure <92169356+damayantiazure@users.noreply.github.com>
2024-06-16 11:14:08 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,8 @@
-// provider "azurerm" {
-//   version = ">=3.0"
-//   # The "feature" block is required for AzureRM provider 2.x. 
-//   features {}
-// }
-// terraform {
-//   required_providers {
-//     azurerm = {
-//       source  = "hashicorp/azurerm"
-//       version = "~> 2.12"
-//     }
-//     azapi = {
-//       source = "Azure/azapi"
-//       version = "0.4.0"
-//     }
-//   }
-// }
-
+provider "azurerm" {
+  version = ">=2.0"
+  # The "feature" block is required for AzureRM provider 2.x. 
+  features {}
+}
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.resource-group-name}-${var.environment}"
@@ -24,22 +11,4 @@ resource "azurerm_resource_group" "rg" {
   tags = {
     environment = var.environment
   }
-}
-
-resource "azapi_resource" "symbolicname" { 
-  type = "Microsoft.Communication/communicationServices@2021-10-01-preview"
-  name = "abncommunicationservicedemo"
-  location = "global"
-  parent_id = "/subscriptions/989f76fd-5edb-4aa1-b014-f989e1918adc/resourceGroups/abndemo-rg"
-  tags = {
-    tagName1 = "demo"
-    tagName2 = "comm"
-  }
-  body = jsonencode({
-    properties = {
-      dataLocation = "europe"
-      linkedDomains = ["/subscriptions/989f76fd-5edb-4aa1-b014-f989e1918adc/resourceGroups/sentinel-rg/providers/Microsoft.Communication/emailServices/testdomaindemonstartionabn"  ]  
-      
-    }
-  })
 }
